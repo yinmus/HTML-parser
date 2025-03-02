03060b6a8578e3716f3593780377f244c0b7f509
kliscle <lachin.akhmetov@gmail.com>
2025-03-02 14:26:18 +0500

    Update server.py

--- server.py
+++ server.py
@@ -5,29 +5,30 @@ import signal
 import socket
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
-def port_use(port):
+
+def port_use(p):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(("localhost", port)) == 0
-def killuport(port):
+        return s.connect_ex(("localhost", p)) == 0
+
+def killuport(p):
     if sys.platform.startswith("linux"):
-        os.system(f"fuser -k {port}/tcp")
+        os.system(f"fuser -k {p}/tcp")
     elif sys.platform.startswith("win"):
-        os.system(f"for /f \"tokens=5\" %i in ('netstat -ano ^| findstr :{port}') do taskkill /F /PID %i")
+        os.system(f"for /f \"tokens=5\" %i in ('netstat -ano ^| findstr :{p}') do taskkill /F /PID %i")
     else:
-        print("Пожалуйста, завершите процесс вручную.")
+        print("Kill process manually.")
+
 def libr_load():
     lib = ctypes.CDLL('./libparser.so')
     lib.parse_html.argtypes = [ctypes.c_char_p]
     lib.parse_js.argtypes = [ctypes.c_char_p]
     lib.parse_css.argtypes = [ctypes.c_char_p]
     return lib
+
 class Hdlr(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        self.html_path = kwargs.pop('html_path', None)
-        self.js_path = kwargs.pop('js_path', None)
-        self.css_path = kwargs.pop('css_path', None)
-        self.lib = kwargs.pop('lib', None)
-        super().__init__(*args, **kwargs)
+    def __init__(self, *a, **kw):
+        self.html_path, self.js_path, self.css_path, self.lib = kw.pop('html_path', None), kw.pop('js_path', None), kw.pop('css_path', None), kw.pop('lib', None)
+        super().__init__(*a, **kw)
     def do_GET(self):
         if self.path == '/' and os.path.exists(self.html_path):
             self.srv_file(self.html_path, "text/html", self.lib.parse_html)
@@ -37,41 +38,43 @@ class Hdlr(SimpleHTTPRequestHandler):
             self.srv_file(self.css_path, "text/css", self.lib.parse_css)
         else:
             super().do_GET()
-    def srv_file(self, file_path, content_type, parse_func):
-        with open(file_path, "r") as f:
-            content = f.read()
-            parse_func(content.encode('utf-8'))
+    def srv_file(self, path, ctype, func):
+        with open(path, "r") as f:
+            d = f.read()
+            func(d.encode('utf-8'))
             self.send_response(200)
-            self.send_header("Content-type", content_type)
+            self.send_header("Content-type", ctype)
             self.end_headers()
-            self.wfile.write(content.encode('utf-8'))
-def stdwm(signum, frame):
-    print("Завершение работы...")
+            self.wfile.write(d.encode('utf-8'))
+
+def stdwm(sig, frame):
+    global httpd
+    print("Exit...")
     httpd.server_close()
     sys.exit(0)
+
 def main():
+    global httpd
     if len(sys.argv) < 3:
-        print("Использование: python server.py <port> <html> [js] [css]")
+        print("Usage: python server.py <port> <html> [js] [css]")
         sys.exit(1)
-    port = int(sys.argv[1])
-    html, js, css = sys.argv[2], sys.argv[3] if len(sys.argv) > 3 else None, sys.argv[4] if len(sys.argv) > 4 else None
-    if port_use(port):
-        choice = input(f"Порт {port} используется. Завершить процесс? [y/n]: ").strip().lower()
-        if choice == 'y':
-            killuport(port)
-            while port_use(port):
+    p, html, js, css = int(sys.argv[1]), sys.argv[2], sys.argv[3] if len(sys.argv) > 3 else None, sys.argv[4] if len(sys.argv) > 4 else None
+    if port_use(p):
+        if input(f"Port {p} in use. Kill process? [y/n]: ").strip().lower() == 'y':
+            killuport(p)
+            while port_use(p):
                 pass
         else:
             sys.exit(1)
     lib = libr_load()
     signal.signal(signal.SIGINT, stdwm)
     signal.signal(signal.SIGTERM, stdwm)
-    handler = lambda *args, **kwargs: Hdlr(*args, **kwargs, html_path=html, js_path=js, css_path=css, lib=lib)
-    httpd = TCPServer(("", port), handler)
-    print(f"Сервер запущен на http://localhost:{port}")
+    httpd = TCPServer(("", p), lambda *a, **kw: Hdlr(*a, **kw, html_path=html, js_path=js, css_path=css, lib=lib))
+    print(f"Server running on http://localhost:{p}")
     try:
         httpd.serve_forever()
     except KeyboardInterrupt:
         stdwm(None, None)
+
 if __name__ == "__main__":
     main()
